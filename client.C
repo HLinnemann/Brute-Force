920d2ded1ac27999683b83f4277bf4581adc700b
M4XB01 <henrik.linnemann@fh-bielefeld.de>
2021-12-15 16:23:53 +0100

    innitialer upload

---
+++ client.C
@@ -0,0 +1,48 @@
+/*
+ * client.C
+ *
+ *  Created on: 11.09.2019
+ *      Author: aml
+ */
+
+#include <string>
+#include <iostream>
+#include <unistd.h> //contains various constants
+
+#include "SIMPLESOCKET.H"
+
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	TCPclient c;
+	string host = "localhost";
+	string msg;
+
+	//connect to host
+	c.conn(host,2035);
+
+	msg = string("Test");
+		cout << "client sends:" << msg << endl;
+	c.sendData(msg);
+	msg = c.receive(10);
+	cout << "got response:" << msg << endl;
+	/*int i=0;
+	bool goOn=1;
+	while(goOn){ // send and receive data
+		if((rand() % 20) < i++){
+			msg = string("BYEBYE");
+			goOn = 0;
+		}else{
+			msg = string("client wants this");
+		}
+		cout << "client sends:" << msg << endl;
+		c.sendData(msg);
+		msg = c.receive(32);
+		cout << "got response:" << msg << endl;
+		sleep(1);
+
+	}*/
+}
+
+
---
+++ server.C
@@ -0,0 +1,77 @@
+/*
+ * server.C
+ *
+ *  Created on: 11.09.2019
+ *      Author: aml
+ */
+#include <cstdio> // standard input and output library
+#include <cstdlib> // this includes functions regarding memory allocation
+#include <cstring> // contains string functions
+#include <cerrno> //It defines macros for reporting and retrieving error conditions through error codes
+#include <ctime> //contains various functions for manipulating date and time
+
+#include <unistd.h> //contains various constants
+#include <sys/types.h> //contains a number of basic derived types that should be used whenever appropriate
+#include <arpa/inet.h> // defines in_addr structure
+#include <sys/socket.h> // for socket creation
+#include <netinet/in.h> //contains constants and structures needed for internet domain addresses
+
+#include "SIMPLESOCKET.H"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class MyServer : public TCPserver{
+public:
+		MyServer(int portNmb, int maxDataRecv) : TCPserver(portNmb, maxDataRecv){;};
+
+
+protected:
+		string myResponse(string input);
+		};
+int main(){
+	srand(time(nullptr));
+	MyServer srv(2037,25);
+	srv.run();
+}
+
+string MyServer::myResponse(string input){
+
+	if (input.compare(0,10,"NewPassword")==0)
+	{
+		int	pwdl = input.at(13)-48;
+		int	pwda = input.at(15)-48;
+	}
+	else if (input.compare(0,12,"CheckPassword")==0)
+	{
+		int pwdlaenge = input.length - 15;
+
+		//HülseCode (HenrikHandy)
+
+		//Hash-Umwandlung noch einzufügen
+
+		/*if (pwdo.compare(0,pwdt.length,pwdt)==0)
+		{
+				return string("Access Granted!");
+		}else
+		{
+			return("Access Denied!");
+		}*/
+			}
+}
+
+
+
+
+		// Umwandlung erhaltenes Passwort in Hash-Wert
+
+
+
+
+
+
+/*string MyServer::createPassword(int length, int sign)
+{
+	return 0;
+}
+*/
